d2797c47f08f1b0a77d2b9370fa5e88f6f6af99d
pssantosh <ps.santosh3@gmail.com>
2017-05-15 16:33:05 +0530

    corrected build error

--- JenkinsDemo/Program.cs
+++ JenkinsDemo/Program.cs
@@ -11,7 +11,7 @@ namespace JenkinsDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Jenkins");
-            Console.WriteLine("A new venture into jenkins automation")
+            Console.WriteLine("A new venture into jenkins automation");
         }
     }
 }
